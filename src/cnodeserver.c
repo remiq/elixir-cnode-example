4dbac73846952aa87f7f51fba65d24bb612ccb60
Remigiusz Jackowski <remigiusz.jackowski@gmail.com>
2015-12-10 20:16:25 +0100

    Reverted C changes to first version

--- src/cnodeserver.c
+++ src/cnodeserver.c
@@ -38,45 +38,44 @@ int main(int argc, char **argv) {
   if (erl_publish(port) == -1)
     erl_err_quit("erl_publish");
 
-  while (loop) {
-	  if ((fd = erl_accept(listen, &conn)) == ERL_ERROR)
-	    erl_err_quit("erl_accept");
-	  fprintf(stderr, "Connected to %s\n\r", conn.nodename);
+  if ((fd = erl_accept(listen, &conn)) == ERL_ERROR)
+    erl_err_quit("erl_accept");
+  fprintf(stderr, "Connected to %s\n\r", conn.nodename);
 
+  while (loop) {
 
     got = erl_receive_msg(fd, buf, BUFSIZE, &emsg);
     if (got == ERL_TICK) {
       /* ignore */
     } else if (got == ERL_ERROR) {
-      fprintf(stderr, "error received, listening now\n\r");
-      //loop = 0;
+      loop = 0;
     } else {
 
       if (emsg.type == ERL_REG_SEND) {
-	fromp = erl_element(2, emsg.msg);
-	tuplep = erl_element(3, emsg.msg);
-	fnp = erl_element(1, tuplep);
-	argp = erl_element(2, tuplep);
+        fromp = erl_element(2, emsg.msg);
+        tuplep = erl_element(3, emsg.msg);
+        fnp = erl_element(1, tuplep);
+        argp = erl_element(2, tuplep);
 
-	if (strncmp(ERL_ATOM_PTR(fnp), "foo", 3) == 0) {
-	  res = foo(ERL_INT_VALUE(argp));
-	} else if (strncmp(ERL_ATOM_PTR(fnp), "bar", 3) == 0) {
-	  res = bar(ERL_INT_VALUE(argp));
-	}
+        if (strncmp(ERL_ATOM_PTR(fnp), "foo", 3) == 0) {
+          res = foo(ERL_INT_VALUE(argp));
+        } else if (strncmp(ERL_ATOM_PTR(fnp), "bar", 3) == 0) {
+          res = bar(ERL_INT_VALUE(argp));
+        }
 
-	resp = erl_format("{cnode, ~i}", res);
-	erl_send(fd, fromp, resp);
+        resp = erl_format("{cnode, ~i}", res);
+        erl_send(fd, fromp, resp);
 
-	erl_free_term(emsg.from); erl_free_term(emsg.msg);
-	erl_free_term(fromp); erl_free_term(tuplep);
-	erl_free_term(fnp); erl_free_term(argp);
-	erl_free_term(resp);
+        erl_free_term(emsg.from); erl_free_term(emsg.msg);
+        erl_free_term(fromp); erl_free_term(tuplep);
+        erl_free_term(fnp); erl_free_term(argp);
+        erl_free_term(resp);
       }
     }
   } /* while */
 }
 
-  
+
 int my_listen(int port) {
   int listen_fd;
   struct sockaddr_in addr;
